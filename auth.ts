b0451bcfb0e1ea86fb46e5d0c3c64d083592c4db
fatelexx <fatelexx@outlook.com>
2024-03-19 14:20:06 +0800

    chore: auth prisma usage

--- auth.ts
+++ auth.ts
@@ -2,14 +2,18 @@ import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
-import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
+import { db } from './db';
  
 async function getUser(email: string): Promise<User | undefined> {
     try {
-      const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-      return user.rows[0];
+      const user: User = await db.users.findUniqueOrThrow({
+        where: {
+          email
+        }
+      })
+      return user;
     } catch (error) {
       console.error('Failed to fetch user:', error);
       throw new Error('Failed to fetch user.');
