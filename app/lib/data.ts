8645c99322b451d5636ca87942bd4961b905a0da
fatelexx <fatelexx@outlook.com>
2024-04-15 14:14:14 +0800

    chore: filtered invoice using prisma raw query

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -126,7 +126,7 @@ export async function fetchFilteredInvoices(
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
-    const invoices = await sql<InvoicesTable>`
+    const invoices = await db.$queryRaw<InvoicesTable>`
       SELECT
         invoices.id,
         invoices.amount,
@@ -147,7 +147,7 @@ export async function fetchFilteredInvoices(
       LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
     `;
 
-    return invoices.rows;
+    return invoices;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch invoices.');
